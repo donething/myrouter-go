fe8cac2a163ddab7141594c4aed89f17fa9dcc6e
Donething <donething.net@gmail.com>
2022-09-22 14:57:42 +0800

    增加等待时长，以成功发送IP

--- myrouter.sh
+++ myrouter.sh
@@ -3,7 +3,7 @@
 # 在路由器中自启动脚本，参考 README.md 中自启动的部分
 
 echo "将在N秒后，等路由器连上网络，即开始执行脚本……"
-sleep 60
+sleep 120
 cd /home/donet/bin/myrouter || exit
 chmod +x ./myrouter
 nohup ./myrouter >/dev/null 2>errs.log &
