bdac08edf23a9357d0e0e7fb0387570a7e77e34e
Donething <donething.net@gmail.com>
2024-01-21 04:31:28 +0800

    修改规则列表的样式

--- myrouter.sh
+++ myrouter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ### 在路由器中自启动脚本，适用于 RedmiAx6000
+## 注意执行本脚本前面要用`sh`：`sh ./myrouter.sh`
 # 1. 将本脚本上传到路由器上，注意文件名要为`myrouter.sh`，和下面的完整执行路径一致
 # 2. 需在路由器中打开`/data/auto_ssh/auto_ssh.sh`，
 # 3. 在其最后一行添加`sh /data/myrouter/myrouter.sh`
